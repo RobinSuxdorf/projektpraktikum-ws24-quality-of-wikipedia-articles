02d2ab02dedf7a34ecd2b61e96264d7c2bcd37e2
Robin <robin.suxdorf@t-online.de>
2024-10-26 22:51:16 +0200

    Added doc strings

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -7,6 +7,19 @@ def load_data(
     promo_file_path: str, 
     transform: Callable[[pd.Series], str] | None = None
 ) -> pd.DataFrame:
+    """
+    Load, merges and optionally transforms promotional and non-promotional wikipedia articles.
+
+    Args:
+        good_file_path (str): Path to the CSV file containing the 'good' articles.
+        promo_file_path (str): Path to the CSV file containing the 'promotional' articles.
+        transform (Callable[[pd.Series], str] | None): A function that applies 
+            a transformation to each row of the DataFrame. Expected to return a 
+            string, which will be used as the value for the "text" column.
+
+    Returns:
+        (pd.DataFrame): A combined DataFrame with data from both files, labeled and optionally transformed.
+    """
     try:
         good_df = pd.read_csv(good_file_path, index_col=0)
         promo_df = pd.read_csv(promo_file_path, index_col=0)
@@ -16,9 +29,9 @@ def load_data(
     good_df["label"] = 0
     promo_df["label"] = 1
 
-    df = pd.concat((good_df, promo_df), axis=0).reset_index(drop=True)
+    df = pd.concat((good_df, promo_df), axis=0, ignore_index=True)
 
     if transform:
         df["text"] = df.apply(transform, axis=1)
 
-    return df
+    return df[["text", "label"]]
--- src/data/wikipedia_article_dataset.py
+++ src/data/wikipedia_article_dataset.py
@@ -4,24 +4,52 @@ from torch.utils.data import Dataset
 
 
 class WikipediaArticleDataset(Dataset):
+    """
+    A custom Dataset for Wikipedia articles and their labels, with optional transformations applied to each article.
+
+    Args:
+        articles (list[str]): A list of articles represented as strings.
+        labels (list[int]): A list of integer labels corresponding to each article.
+        encode (Callable[[str], list[int]]): A function for encoding the articles.
+        transform (Callable[[str], str] | None): A function for transforming the articles.
+    """
     def __init__(
         self,
         articles: list[str],
         labels: list[int],
-        transform: Callable[[str], list[int]]
+        encode: Callable[[str], list[int]],
+        transform: Callable[[str], str] | None = None
     ) -> None:
         self._articles = articles
         self._labels = labels
+        self._encode = encode
         self._transform = transform
 
     def __len__(self) -> int:
+        """
+        Returns the number of articles in the dataset.
+
+        Returns:
+            int: The total number of articles.
+        """
         return len(self._articles)
 
     def __getitem__(self, idx: int) -> tuple[torch.LongTensor, torch.Tensor]:
+        """
+        Retrieves a transformed article and its label as tensors.
+
+        Args:
+            idx (int): The index of the article to retrieve.
+
+        Returns:
+            tuple[torch.LongTensor, torch.Tensor]: A tuple containing the article as tensor and the label as tensor.
+        """
         article = self._articles[idx]
         label = self._labels[idx]
 
         if self._transform:
             article = self._transform(article)
 
-        return torch.LongTensor(article), torch.Tensor(label)
+        encoded = self._encode(article)
+
+        return torch.LongTensor(encoded), torch.Tensor(label)
