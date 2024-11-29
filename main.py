7821cd7137b14065cd2998851c912f181be3ad65
Robin <robin.suxdorf@t-online.de>
2024-11-29 17:47:04 +0100

    Added grid hyperparameter optimization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import tiktoken
 import torch
 import torch.nn as nn
@@ -6,21 +7,24 @@ from src.data import load_data, get_data_loaders
 from src.model import TextCNN, train_one_epoch, evaluate_model
 
 
-number_of_elements = 100
-embedding_dim = 256
-num_filters = 100
-filter_sizes = [3, 4, 5]
-max_length = 400
+# number_of_elements = 100
 num_classes = 2
 tokenizer = tiktoken.get_encoding("cl100k_base")
 num_epochs = 5
-batch_size = 32
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def main() -> None:
-    df = load_data("../good.csv", "../promotional.csv")
-    df = df.head(number_of_elements)
+
+def train_run(
+    data: pd.DataFrame,
+    max_length,
+    batch_size,
+    embedding_dim,
+    num_filters,
+    filter_sizes
+):
+    df = data.copy(deep=True)
+    # df = df.head(number_of_elements)
 
     train_loader, test_loader = get_data_loaders(df, tokenizer.encode, max_length, batch_size=batch_size, device = device)
 
@@ -37,12 +41,41 @@ def main() -> None:
 
     criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(num_epochs):
+    for _ in range(num_epochs):
         avg_loss = train_one_epoch(model, train_loader, criterion, optimizer, device=device)
-        print(f"Epoch {epoch+1}, Loss: {avg_loss:.4f}")
         
         acc = evaluate_model(model, test_loader, device=device)
-        print(f"Test Accuracy: {acc:.4f}")
+
+    return avg_loss, acc
+
+def main() -> None:
+    df = load_data("../good.csv", "../promotional.csv")
+
+    embedding_dim_list = [256, 512, 1024]
+    num_filters_list = [50, 100, 150, 200]
+    filter_sizes_list = [[3, 4, 5], [3, 5, 7]]
+    max_length_list = [200, 400, 800]
+    batch_size_list = [32, 64]
+
+    for embedding_dim in embedding_dim_list:
+        for num_filters in num_filters_list:
+            for filter_sizes in filter_sizes_list:
+                for max_length in max_length_list:
+                    for batch_size  in batch_size_list:
+                        loss, acc = train_run(df, max_length, batch_size, embedding_dim, num_filters, filter_sizes)
+
+                        print(loss, acc)
+
+                        with open("results.txt", "a") as f:
+                            f.write(f"Embedding dimension: {embedding_dim}\n")
+                            f.write(f"Num filters: {num_filters}\n")
+                            f.write(f"Filter sizes: {filter_sizes}\n")
+                            f.write(f"Max length: {max_length}\n")
+                            f.write(f"Batch size: {batch_size}\n")
+                            f.write(f"Loss: {loss}\n")
+                            f.write(f"Accuracy: {acc}\n")
+                            f.write("\n")
+    
 
 if __name__ == "__main__":
     main()
